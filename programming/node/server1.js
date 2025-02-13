420ac0df842959aecff2aa23c7f5b1acdea0a599
Ziggismalls <frantz?o]icloud.com>
2025-02-13 17:43:30 +0100

    up

--- programming/node/server1.js
+++ programming/node/server1.js
@@ -1,36 +1,28 @@
 const http = require("http");
 const fs = require("fs");
-const path = require("path");
 
 const routes = {
   "/about": "about.html",
   "/contact": "contact.html",
   "/shop": "shop.html",
+  "/": "index.html",
 };
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     Object.entries(routes).forEach(([path, file]) => {
-      console.log(routes);
-
-      fs.readFile(file, "utf8", (err, data) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end("Erreur interne du serveur");
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(data);
-        }
-      });
-    });
-  } else if (req.url === "/" && req.method === "GET") {
-    fs.readFile("index.html", "utf8", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Erreur interne du serveur");
-      }
-      res.writeHead(200, { "Content-Type": "text.html" });
-      res.end(data);
+      console.log(path);
+      if (req.url === path)
+        fs.readFile(file, "utf8", (err, data) => {
+          if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Erreur interne du serveur");
+          } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+          }
+          return;
+        });
     });
   }
 });
