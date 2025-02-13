c06acf63132bfa81e0df44bf1bb2080b0baca3a1
frantz-tech <frantz.o@icloud.com>
2025-02-13 08:35:46 +0100

    up

--- programming/node/server.js
+++ programming/node/server.js
@@ -4,24 +4,27 @@ const { json } = require("stream/consumers");
 
 // fonction call back en paramètre
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
   // 200 : le code de statut HTTP (200 = succès).
   // "Content-Type": "text/plain" : le type de contenu de la réponse (text/plain = texte brut).
-  res.end("Hello, World!\n");
-});
 
-fs.readFile("folder.json", "utf8", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    try {
-      const json = JSON.parse(data);
-      console.log("Données du fichier Json", json);
-    } catch (error) {
-      console.log("Erreur de parsing JSON", error);
+  fs.readFile("folder.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Erreur interne du serveur");
+    } else {
+      try {
+        const jsonData = JSON.parse(data);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(jsonData));
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Error");
+        console.log("Erreur de parsing JSON", error);
+      }
+      res.end(JSON.parse(data));
     }
-    res.end(JSON.parse(data));
-  }
+  });
 });
 const PORT = 3000;
 server.listen(PORT, () => {
