2ea96a51fa6126a10ed6d57f4d8aad0df1a3a042
frantz-tech <frantz.o@icloud.com>
2025-02-14 08:41:20 +0100

    up

--- programming/node/server1.js
+++ programming/node/server1.js
@@ -11,7 +11,6 @@ const routes = {
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     Object.entries(routes).forEach(([path, file]) => {
-<<<<<<< HEAD
       console.log(routes);
       console.log(path);
       console.log(file);
@@ -35,20 +34,6 @@ const server = http.createServer((req, res) => {
       }
       res.writeHead(200, { "Content-Type": "text.html" });
       res.end(data);
-=======
-      console.log(path);
-      if (req.url === path)
-        fs.readFile(file, "utf8", (err, data) => {
-          if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Erreur interne du serveur");
-          } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-          return;
-        });
->>>>>>> 420ac0df842959aecff2aa23c7f5b1acdea0a599
     });
   }
 });
